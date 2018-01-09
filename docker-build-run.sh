5660309acf14e01754be77da0a311dd32335f722
Jöran Tesse <joeran.tesse@iteratec.de>
2018-01-09 14:27:23 +0100

    full path (ugh!)

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t krakr .
-docker run -v ./krakr:/root/user-store --name krakr -d -p 8070:8070 krakr
+docker run -v /var/krakr:/root/user-store --name krakr -d -p 8070:8070 krakr
